74cc7c5adc1ee0e7c051c242a8e11a1e105ce614
Vadim-Vatrya <VatryaVV@gmail.com>
2023-02-11 01:33:04 +0200

    add geolocation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import Weather from './components/weather';
+
+const App = () => {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(
+        `'https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=eb63035653337f135a8d54653000316a'`,
+      )
+        .then(res => res.json())
+        .then(result => {
+          setData(result);
+          console.log(result);
+        });
+    };
+
+    console.log('Latitude is:', lat);
+    console.log('Longitude is:', long);
+    fetchData();
+  }, [lat, long]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {typeof data.main != 'undefined' ? (
+        <Weather weatherData={data} />
+      ) : (
+        <div></div>
+      )} */}
     </div>
   );
-}
+};
 
 export default App;

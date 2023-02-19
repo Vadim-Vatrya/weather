5026dcc7681e09f30a71f7c5256d82cce2acc3a7
Vadim-Vatrya <VatryaVV@gmail.com>
2023-02-19 23:53:23 +0200

    add responce download image

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,4 +1,30 @@
-import { Context } from 'react';
+import { useContext } from 'react';
 import { WeatherContext } from '../context/WeatherContext';
 import { SEARCH_BY_WORD } from '../api/imageApi';
-import fetchData from '';
+import fetchData from '../utils/fetchData';
+
+const useImgFetch = () => {
+  const { weather, setBgImg } = useContext(WeatherContext);
+
+  const searchImgByWord = word => {
+    fetchData(`${SEARCH_BY_WORD}${word}`).then(res => {
+      setBgImg(
+        res?.data.results[Math.floor(Math.random() * 10)]?.urls?.regular,
+      );
+
+      if (res?.data.results.length === 0 || res?.data.results.length < 10) {
+        fetchData(
+          `${SEARCH_BY_WORD}${weather?.weather?.[0]?.description}`,
+        ).then(res => {
+          setBgImg(
+            res?.data.results[Math.floor(Math.random() * 10)]?.urls?.regular,
+          );
+        });
+      }
+    });
+  };
+
+  return searchImgByWord;
+};
+
+export default useImgFetch;
